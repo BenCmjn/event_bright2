0fc4ce3422907e1a76423fa0bd47eac418a6b1e0
Yann <yann120@hotmail.fr>
2018-02-05 20:02:14 +0100

    before cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root 'static_pages#home'
+
+  get 'static_pages/home'
+
+  get 'static_pages/secret', as: 'secret'
+
+  get 'users/new', as: 'new_user'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
