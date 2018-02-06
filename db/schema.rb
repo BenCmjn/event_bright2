34ae9c77472d377bffdac2c06d9d174a662837ad
Yann <yann120@hotmail.fr>
2018-02-06 17:55:00 +0100

    add cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205113832) do
+ActiveRecord::Schema.define(version: 20180206135531) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
